6963034013bd176b183cb8304cdbb3fdb1a042b6
taha0012 <taha0012@algonquinlive.com>
2016-02-02 09:52:32 -0500

    added files

---
+++ js/push-menu.js
@@ -0,0 +1,19 @@
+// Needs latest version of jQuery to run
+
+$(function(){
+  $('body').removeClass('noscript');
+
+$('.toggle-btn, .close-btn').click(function(){
+  togglenav();
+
+  });
+});
+function togglenav(){
+  if($('.site-wrapper').attr('data-state') == 'slide-closed'){
+
+$('.site-wrapper').attr('data-state','slide-open');
+  }else{
+$('.site-wrapper').attr('data-state','slide-closed');
+
+  }
+}
